4b0350011512b0c817b5d891832a1eb51fc19e99
Jack Sockwell <jacksockwell@jacksockwell.net>
2026-04-08 00:00:08 -0400

    xx

--- supabase-config.js
+++ supabase-config.js
@@ -1,7 +1,7 @@
 window.SUPABASE_CONFIG = Object.assign(
   {
-    url: "",
-    anonKey: "",
+    url: "https://qcfdionrpetayqawvjrt.supabase.co",
+    anonKey: "sb_publishable_gdsHK0H1YOZahDX9ST8oJg_rKeMp8p8",
     projectTable: "portfolio_projects",
     storageBucket: "portfolio-media",
     publicSection: "art",
